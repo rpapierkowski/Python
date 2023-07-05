1b64b005d69f3bfd6da42e81459fb0a70dec0277
Rafał <rpapierkowski@gmail.com>
2023-07-05 12:58:44 +0200

    sa_availability_daily_pipeline
    Files to fill in the datamart

---
+++ sa_availability_daily_pipeline/stating_sa_list.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT(sa.id)
+FROM saved_auctions sa 
+WHERE sa.id NOT IN (SELECT tl.tableid
+FROM total_log tl 
+WHERE tl.table_name_id = 495 
+	AND tl.field_name_id = 747
+	AND tl.updated < '2022-01-01 00:00:00'
+	AND new_value = 1 )
+	AND sa.id BETWEEN 1000 AND 5000
